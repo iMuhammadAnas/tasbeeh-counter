fcae518dbd08dc383a094e1b5c072dd9edc5df31
Muhammad Anas <Anasnaseem420@gmail.com>
2023-01-18 06:42:07 +0500

    counter app using react redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { incrementNumber, decrementNumber } from './actions/index'
 
-function App() {
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="main-div">
+        <div className="container">
+          <h1>Tasbeeh counter</h1>
+          <h4>using React and Redux</h4>
+          <div className="quantity">
+            <a className="quantity__minus" title="Decrement" onClick={ () => dispatch(decrementNumber()) }><span>-</span></a>
+            <input name="quantity" type="text" className="quantity__input" value={myState} />
+            <a className="quantity__plus" title="Increment" onClick={ () => dispatch(incrementNumber()) }><span>+</span></a>
+          </div>
+        </div>
+        <div className="water-mark">Design by <a href="https://github.com/iMuhammadAnas" target="_blank">MUHAMMAD ANAS</a><span>❤</span></div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
